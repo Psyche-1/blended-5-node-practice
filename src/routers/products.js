5ab613469e1bf855329f0727aa787aea660f2379
Artem <suleymanov2007artem@gmail.com>
2025-04-06 11:16:31 +0300

    task-4

--- src/routers/products.js
+++ src/routers/products.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
-import { getProductsController } from "../controllers/products.js";
+import { getProductsController, getProductByIdController } from "../controllers/products.js";
 
 const productsRouter = Router();
 
 productsRouter.get('/', ctrlWrapper(getProductsController));
+productsRouter.get('/:productId', ctrlWrapper(getProductByIdController));
 
 export default productsRouter;
