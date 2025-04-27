26d709b270a5de622e39ecea7f659217f30ba4e6
Mariia Skydan <kyrosika565@gmail.com>
2025-04-27 10:37:11 +0300

    validation

--- src/routers/products.js
+++ src/routers/products.js
@@ -7,13 +7,16 @@ import {
   updateProductByIdController,
   deleteProductByIdController
 } from '../controllers/products.js';
+import { validateBody } from '../utils/validateBody.js';
+import { addProductSchema, editProductSchema } from '../validation/product.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const productsRouter = Router();
 
 productsRouter.get('/', ctrlWrapper(getProductsController));
-productsRouter.get('/:productId', ctrlWrapper(getProductByIdController));
-productsRouter.post('/', ctrlWrapper(addProductController));
-productsRouter.patch('/:productId', ctrlWrapper(updateProductByIdController));
-productsRouter.delete('/:productId', ctrlWrapper(deleteProductByIdController));
+productsRouter.get('/:productId', isValidId, ctrlWrapper(getProductByIdController));
+productsRouter.post('/', validateBody(addProductSchema), ctrlWrapper(addProductController));
+productsRouter.patch('/:productId', isValidId, validateBody(editProductSchema), ctrlWrapper(updateProductByIdController));
+productsRouter.delete('/:productId', isValidId, ctrlWrapper(deleteProductByIdController));
 
 export default productsRouter;
