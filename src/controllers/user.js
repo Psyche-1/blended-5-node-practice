c0cd2da5f98a0ce51a84ba4c35451c23383fdc74
Angelina Simora <angelinasimora997@gmail.com>
2025-04-27 11:57:30 +0200

    login

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,5 @@
-import { register } from '../services/users.js';
+import { ONE_DAY } from '../constants/index.js';
+import { register, login } from '../services/users.js';
 
 export const registerController = async (req, res) => {
   const data = await register(req.body);
@@ -6,5 +7,29 @@ export const registerController = async (req, res) => {
     status: 201,
     message: 'Successfully register user',
     data,
+  });
+};
+
+const setUpSession = (res, session) => {
+    res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+};
+
+export const loginController = async (req, res) => {
+  const session = await login(req.body);
+  setUpSession(res, session);
+
+
+  res.json({
+    status: 201,
+    message: 'Successfully register user',
+    data: { accessToken: session.accessToken, },
+
   });
 };
